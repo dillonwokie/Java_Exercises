5310b0359d96d7689ddf5bbb9fe2af42a6205449
dillonwokie <dillonwokie@gmail.com>
2024-11-21 22:10:49 +0800

    added new methods

--- sumAverageRunningInt/src/Main.java
+++ sumAverageRunningInt/src/Main.java
@@ -13,6 +13,10 @@ public class Main {
         int upperBound = Integer.parseInt(scan.nextLine());
 
         System.out.println("The sum of " + lowerBound + " to " + upperBound + " is " + main.sumRunningInt(lowerBound, upperBound));
+        System.out.println("The sum of even numbers from " + lowerBound + " to " + upperBound + " is " + main.sumOfEvenRunningInt(lowerBound, upperBound));
+        System.out.println("The sum of odd numbers from " + lowerBound + " to " + upperBound + " is " + main.sumOfOddRunningInt(lowerBound, upperBound));
+        System.out.println("The absolute difference of even and odd numbers from " + lowerBound + " to " + upperBound + " is " + main.absoluteDifference(lowerBound, upperBound));
+        System.out.println("The squared sum of " + lowerBound + " to " + upperBound + " is " + main.sumOfSquares(lowerBound, upperBound));
         System.out.println("The average is " + main.averageRunningInt(lowerBound, upperBound));
     }
 
@@ -25,8 +29,40 @@ public class Main {
         return sum;
     }
 
+    int sumOfOddRunningInt(int lowerBound, int upperBound) {
+        int sum = 0;
+        for (int i = lowerBound; i <= upperBound; i++) {
+            if (i % 2 != 0) {
+                sum += i++;
+            }
+        }
+        return sum;
+    }
+
+    int sumOfEvenRunningInt(int lowerBound, int upperBound) {
+        int sum = 0;
+        for (int i = lowerBound; i <= upperBound; i++) {
+            if (i % 2 == 0) {
+                sum += i++;
+            }
+        }
+        return sum;
+    }
+
+    int absoluteDifference(int lowerBound, int upperBound) {
+        return Math.abs(sumOfEvenRunningInt(lowerBound, upperBound) - sumOfOddRunningInt(lowerBound, upperBound));
+    }
+
     double averageRunningInt(int lowerBound, int upperBound) {
         return (double) (lowerBound + upperBound) / 2;
+    }
+
+    int sumOfSquares(int lowerBound, int upperBound) {
+        int sum = 0;
+        for (int i = lowerBound; i <= upperBound; i++) {
+            sum += (int) Math.pow(lowerBound++, 2);
+        }
+        return sum;
     }
 
 }
