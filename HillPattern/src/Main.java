54b8339949c5ec8dd9ecda109d89bbd4a918b9cc
dillonwokie <dillonwokie@gmail.com>
2024-12-09 17:38:09 +0800

    HillPattern Program part a to c

---
+++ HillPattern/src/Main.java
@@ -0,0 +1,84 @@
+// Write 3 programs called HillPatternX (X = A, B, C, D) that prompts user for the size (a non-negative integer in int); and prints the pattern as shown:
+//
+//          #                # # # # # # # # # # #                #                # # # # # # # # # # #
+//        # # #                # # # # # # # # #                # # #              # # # # #   # # # # #
+//      # # # # #                # # # # # # #                # # # # #            # # # #       # # # #
+//    # # # # # # #                # # # # #                # # # # # # #          # # #           # # #
+//  # # # # # # # # #                # # #                # # # # # # # # #        # #               # #
+//# # # # # # # # # # #                #                # # # # # # # # # # #      #                   #
+//         (a)                        (b)                 # # # # # # # # #        # #               # #
+//                                                          # # # # # # #          # # #           # # #
+//                                                            # # # # #            # # # #       # # # #
+//                                                              # # #              # # # # #   # # # # #
+//                                                                #                # # # # # # # # # # #
+//                                                               (c)                        (d)
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the rows: ");
+        int numRows = 6;
+        input.close();
+
+        // Pattern A
+        System.out.println("Pattern A: ");
+        int numRowsA = numRows;
+        int numColsA = 2 * numRowsA - 1;
+        for (int row = 1; row <= numRowsA; row++) {
+            for (int col = 1; col <= numColsA; col++) {
+                if ((row >= col - numRowsA + 1) && (row + col >= numRowsA + 1)) {
+                    System.out.print("# ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        // Pattern B
+        System.out.println("Pattern B: ");
+        int numRowsB = numRows;
+        int numColsB = 2 * numRowsB - 1;
+        for (int row = numRowsB; row >= 1; row--) {
+            for (int col = numColsB; col >= 1; col--) {
+                if ((row >= col - numRowsB + 1) && (row + col >= numRowsB + 1)) {
+                    System.out.print("# ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        // Pattern C
+        System.out.println("Pattern C: ");
+        int numRowsC = 2 * numRows - 1;
+        for (int row = 1; row <= numRows; row++) {
+            for (int col = 1; col <= numRowsC; col++) {
+                if (col >= numRows - row + 1 && col <= numRows + row - 1) {
+                    System.out.print("# ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        for (int row = numRows - 1; row >= 1; row--) {
+            for (int col = 1; col <= numRowsC; col++) {
+                if (col >= numRows - row + 1 && col <= numRows + row - 1) {
+                    System.out.print("# ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+    }
+}
+
